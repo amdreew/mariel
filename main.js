364dd4e93b325e66a47104a4bf9d3aecd4a72194
miguel andres ospino <33471145+amdreew@users.noreply.github.com>
2023-02-10 12:22:51 -0500

    ajusste chrome

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
 https://docs.spring.io/spring-security/reference/servlet/authentication/session-management.html
+
+"C:\Program Files\Google\Chrome\Application\chrome.exe" --disable-web-security --user-data-dir="C:/ChromeDevSession"
